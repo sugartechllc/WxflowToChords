6e122176806086efb84f03b00e3f4fcd90a7ade7
sugartechllc <sugartechllc@gmail.com>
2017-12-09 19:52:05 +0000

    Remove wipy specialization.

--- WxflowToChords.py
+++ WxflowToChords.py
@@ -1,9 +1,3 @@
-try:
-    import machine
-    import WipyStat
-    on_wipy = True
-except:
-    on_wipy = False
 import json
 import os
 import time
@@ -27,34 +21,26 @@ def run(config_file):
     config = json.loads(open(config_file).read())
     host   = config["chords_host"]
     port   = config["listen_port"]
-    
+
     FromWxflow.startReader(port)
     ToChords.startSender()
-    
-    stat_cycle = 59
-    stat_count = stat_cycle
+
     while True:
+
         collect()
+        sys.stdout.flush()
+        sys.stderr.flush() 
         time.sleep(1)
         wxflow_msgs = FromWxflow.get_msgs()
         for w in wxflow_msgs:
             wxflow_msg = json.loads(w)
             chords_records = DecodeWxflow.toChords(config, wxflow_msg)
-            
+
             if chords_records:
                 for chords_record in chords_records:
                     uri = ToChords.buildURI(host, chords_record)
                     ToChords.submitURI(uri, 60)
-                    #mem_info()
-        # Send Wipy status
-        if on_wipy:
-            stat_count = stat_count + 1
-            if stat_count > stat_cycle:
-                chords_record = WipyStat.toChords(config)
-                uri = ToChords.buildURI(host, chords_record)
-                ToChords.submitURI(uri, 60)
-                stat_count = 0
-    
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print ("Usage:", sys.argv[0], 'config_file')
